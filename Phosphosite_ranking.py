196a5aef9cc86a320cfdf2bcca776d9ac675e97d
Athira PG <94819724+athirapg@users.noreply.github.com>
2024-07-04 12:07:17 +0530

    Update Phosphosite_ranking.py

--- Phosphosite_ranking.py
+++ Phosphosite_ranking.py
@@ -1,13 +1,9 @@
 import pandas as pd
 
-# Create the DataFrame
 df = pd.read_excel(r"Kinase_sitecount.xlsx", 'Sheet2')
 
-
-# Rank each kinase phosphosite based on profiling count
 df['Rank'] = df.groupby('mapped_genesymbol')['exp_condition_count_diff'].rank(ascending=False, method='min')
 
-# Sort by gene symbol and rank
 df = df.sort_values(by=['mapped_genesymbol', 'Rank'])
 
 #print(df)
